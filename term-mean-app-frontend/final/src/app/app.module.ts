3ef6bd0bc5fe5849cba4d10c387aaa361c85671f
abdul.zaidi <abdul.zaidi@fenix.life>
2022-06-04 18:44:26 +0500

    More Components created

--- term-mean-app-frontend/final/src/app/app.module.ts
+++ term-mean-app-frontend/final/src/app/app.module.ts
@@ -5,13 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
-import { UserService } from './services/user.service';
 import { SignupComponent } from './signup/signup.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { DepartmentComponent } from './department/department.component';
+import { ComplaintComponent } from './complaint/complaint.component';
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    HomepageComponent,
+    DepartmentComponent,
+    ComplaintComponent
   ],
   imports: [
     BrowserModule,
